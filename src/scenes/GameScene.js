fabc634c69b8199d217fb43715ba8bd1b746f002
mangobanaani <psilt@iki.fi>
2026-05-03 04:10:32 +0300

    add terrain rendering with heightmap and grass surface

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,9 +1,11 @@
+import Terrain from '../objects/Terrain.js';
+
 export default class GameScene extends Phaser.Scene {
     constructor() {
         super('GameScene');
     }
 
     create() {
-        this.add.text(400, 300, 'Game Scene', { fontSize: '24px', fill: '#333' }).setOrigin(0.5);
+        this.terrain = new Terrain(this);
     }
 }
